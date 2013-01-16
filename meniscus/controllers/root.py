375452e8068f278b2a8f5dc24815b6527fe55ee6
John Hopper <john.hopper@rackspace.com>
2013-01-16 13:53:14 -0600

    figuring out pecan

--- meniscus/controllers/root.py
+++ meniscus/controllers/root.py
@@ -1,29 +1,30 @@
 from pecan import expose, redirect
-from webob.exc import status_map
+from pecan.rest import RestController
+from pecan.core import abort
 
 from meniscus.model import session_maker
-
-
-class RootController(object):
-
-    @expose(generic=True, template='index.html')
-    def index(self):
-        return dict()
-
-    @index.when(method='POST')
-    def index_post(self, q):
-        redirect('http://pecan.readthedocs.org/en/latest/search.html?q=%s' % q)
-
-    @expose('error.html')
-    def error(self, status):
-        try:
-            status = int(status)
-        except ValueError:  # pragma: no cover
-            status = 500
-        message = getattr(status_map.get(status), 'explanation', '')
-        return dict(status=status, message=message)
 
 class TennantController(object):
 
     @expose()
     def index(self):
+        abort(404)
+
+class RootController(object):
+
+    tennant = TennantController()
+
+    @expose('json')
+    def error(self, status):
+        try:
+            status = int(status)
+        except ValueError:
+            status = 0
+
+        message = None
+
+        if status == 404:
+            message = 'not found'
+            
+        return dict(status=status, message=message)
+

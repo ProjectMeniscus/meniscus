4e1be4f4c5ea1c7dcb6a9f05920877085a5d6a87
taz <aelkikhia@gmail.com>
2013-02-26 08:02:22 -0600

    added node config and tests

---
+++ meniscus/api/cfg/resources.py
@@ -0,0 +1,43 @@
+import falcon
+import unittest
+import json
+
+from meniscus.api import ApiResource, abort
+
+
+def _header_not_valid():
+    abort(falcon.HTTP_401, 'unauthorized request')
+
+
+def _role_not_valid():
+    abort(falcon.HTTP_403, 'roles do not have access to this resource')
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(NodeConfigurationResource())
+
+
+class NodeConfigurationResource(ApiResource):
+
+    def on_get(self, req, resp):
+
+        VALID_HEADER = 'X-Auth-Roles'
+        VALID_ROLE = 'meniscus_role'
+
+        roles = req.get_header(VALID_HEADER)
+
+        if not roles:
+            _header_not_valid()
+
+        has_access = False
+
+        for role in roles:
+            if role == VALID_ROLE:
+                has_access = True
+                break
+
+        if has_access:
+            resp.status = falcon.HTTP_200
+        else:
+            _role_not_valid()
--- meniscus/node/node_config.py
+++ meniscus/node/node_config.py
@@ -1,6 +1,5 @@
-import unittest
 from oslo.config import cfg
-from meniscus.config import init_config, get_config
+from meniscus.config import get_config
 
 # Node configuration options
 node_group = cfg.OptGroup(name='node_cfg',
@@ -11,30 +10,9 @@ NODE_OPTIONS = [
     cfg.BoolOpt('verbose', default=False, ),
     cfg.StrOpt('personality', default='default_personality',
                help="""Node personality"""),
-    cfg.StrOpt('name', default='default_name', help="""Node name""" )
+    cfg.StrOpt('name', default='default_name', help="""Node name""")
 ]
 
 get_config().register_opts(NODE_OPTIONS, group=node_group)
 
 
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(WhenConfiguring())
-
-
-class WhenConfiguring(unittest.TestCase):
-
-    def test_loading(self):
-
-        init_config(['--config-file', '../../node.cfg.example'])
-
-        conf = get_config()
-        conf.register_group(node_group)
-        conf.register_opts(NODE_OPTIONS, group=node_group)
-
-        self.assertTrue(conf.node_cfg.verbose)
-        self.assertEqual(conf.node_cfg.personality, 'cfg_node_personality')
-        self.assertEqual(conf.node_cfg.name, 'cfg_node_name')
-
-# if __name__ == '__main__':
-#     unittest.main()

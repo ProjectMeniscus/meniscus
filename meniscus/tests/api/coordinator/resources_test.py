375867e053902eb9049246e6d84aa55dc2fb6c6a
taz <aelkikhia@gmail.com>
2013-03-04 11:26:48 -0600

    added tests for coordinator registration

--- meniscus/tests/api/coordinator/resources_test.py
+++ meniscus/tests/api/coordinator/resources_test.py
@@ -1,4 +1,5 @@
-from meniscus.api.coordinator.resources import NodeConfigurationResource
+from meniscus.api.coordinator.resources import WorkerConfigurationResource
+from meniscus.api.coordinator.resources import WorkerRegistrationResource
 from mock import MagicMock
 
 import falcon
@@ -7,17 +8,57 @@ import unittest
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(NodeConfigurationResource())
-
+    suite.addTest(WorkerConfigurationResource())
+    suite.addTest(WorkerRegistrationResource())
     return suite
 
 
-class WhenTestingNodeConfigurationResource(unittest.TestCase):
+class WhenTestingWorkerRegistration(unittest.TestCase):
+
+    def setUp(self):
+        self.stream = MagicMock()
+        self.resp = MagicMock()
+        self.resource = WorkerRegistrationResource()
+        self.req = MagicMock()
+        self.req.stream = self.stream
+
+    def test_should_return_202_on_get(self):
+        self.stream.read.return_value = \
+            u'{"event" : { "type": "worker.registration",\
+            "callback": "192.168.100.101:8080/v1/configuration/"},\
+            "worker": { "hostname" : "worker-n01",\
+            "ip_address_v4": "192.168.100.101",\
+            "ip_address_v6": "::1",\
+            "personality": "correlation|normalization|storage",\
+            "system_info": [{"disk_gb" : "20",\
+                            "os_type" : "Darwin-11.4.2-x86_64-i386-64bit",\
+                            "memory_mb" : "1024",\
+                            "architecture" : ""}]}}'
+        self.resource.on_post(self.req, self.resp)
+        self.assertEqual(falcon.HTTP_202, self.resp.status)
+
+    def test_invalid_event_type_return_401_on_get(self):
+        self.stream.read.return_value = \
+            u'{"event" : { "type": "worker.monkey",\
+            "callback": "192.168.100.101:8080/v1/configuration/"},\
+            "worker": { "hostname" : "worker-n01",\
+            "ip_address_v4": "192.168.100.101",\
+            "ip_address_v6": "::1",\
+            "personality": "correlation|normalization|storage",\
+            "system_info": [{"disk_gb" : "20",\
+                            "os_type" : "Darwin-11.4.2-x86_64-i386-64bit",\
+                            "memory_mb" : "1024",\
+                            "architecture" : ""}]}}'
+        with self.assertRaises(falcon.HTTPError):
+            self.resource.on_post(self.req, self.resp)
+
+
+class WhenTestingWorkerConfigurationResource(unittest.TestCase):
 
     def setUp(self):
         self.req = MagicMock()
         self.resp = MagicMock()
-        self.resource = NodeConfigurationResource()
+        self.resource = WorkerConfigurationResource()
 
     def test_should_return_401_on_get(self):
         self.req.get_header.return_value = ''

25e37479e1b977ce82dd8888210eccd0dd54b90a
Steven Gonzales <stevendgonzales@gmail.com>
2013-03-08 15:53:07 -0600

    update unit tests for changes made in code

--- meniscus/tests/app_test.py
+++ meniscus/tests/app_test.py
@@ -10,7 +10,7 @@ class TestingApIBootstrap(unittest.TestCase):
         self.cache = MagicMock()
         self.cache.cache_exists.return_value = True
         self.cache.cache_get.return_value = \
-            {'personality_module': 'meniscus.personas.worker.pairing'}
+            u'{"personality_module": "meniscus.personas.worker.pairing"}'
         self.no_cache = MagicMock()
         self.no_cache.cache_exists.return_value = False
 

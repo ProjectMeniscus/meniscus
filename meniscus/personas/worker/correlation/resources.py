b39bf928e0209584f67abb7d78eb6a29e56ceae6
taz <aelkikhia@gmail.com>
2013-03-15 14:51:53 -0500

    temp build of tenant id flow

--- meniscus/personas/worker/correlation/resources.py
+++ meniscus/personas/worker/correlation/resources.py
@@ -4,18 +4,36 @@ import requests
 
 from meniscus.api import ApiResource, load_body, abort
 from meniscus.api.utils.request import http_request
-from meniscus.data.model.util import find_tenant
+from meniscus.openstack.common import jsonutils
 from meniscus.data.model.tenant import Tenant
+from meniscus.data.model.util import find_host
+from meniscus.data.model.util import find_event_producer_for_host
+from meniscus.data.model.util import load_tenant_from_dict
 from meniscus.proxy import NativeProxy
 
 
 MSG_TOKEN = 'MESSAGE-TOKEN'
 
-def _tenant_not_found():
+
+def _hostname_not_provided():
+    """
+    sends an http 400 response to the caller
+    """
+    abort(falcon.HTTP_400, "malformed request, hostname cannot be empty")
+
+
+def _producer_not_provided():
+    """
+    sends an http 400 response to the caller
+    """
+    abort(falcon.HTTP_400, "malformed request, procname cannot be empty")
+
+
+def _host_not_found():
     """
     sends an http 404 response to the caller
     """
-    abort(falcon.HTTP_404, 'The tenant id specified was not found')
+    abort(falcon.HTTP_404, 'hostname not found for this tenant')
 
 
 def _coordinator_connection_failure():
@@ -39,72 +57,76 @@ class PublishResource():
     def __init__(self, db_handler):
         self.db = db_handler
 
-    def _get_tenant_from_cache(self, tenant_id):
-        """
-        check cache for tenant if it is available
-        """
-        cache = NativeProxy()
-        #todo: not sure if whole tenants are cached or just tenant ids
-        #todo: will do discovery later
-        #todo: refactor of json.loads on cache
-        tenant_list = json.loads(cache.cache_get('tenants'))
-        if tenant_id in tenant_list:
-            return True
-        else:
-            return False
-
-    def _get_tenant_from_coordinator(self, tenant_id, req):
-        """
-        contact the Coordinator API to verify the tenant id
-        """
-        # call to coordinator
-        cache = NativeProxy()
-        config = json.loads(cache.cache_get('worker_configuration'))
-        coordinator_uri = config['coordinator_uri']
-
-        token_header = {"WORKER-TOKEN": config['worker_token'],
-                        "WORKER-ID": config['worker_id']}
-        request_uri = "{0}/{1}/publish".format(
-            coordinator_uri, tenant_id)
-
-        try:
-            resp = http_request(request_uri, token_header,
-                                http_verb='POST')
-            if resp.status_code == httplib.NOT_FOUND:
-                #todo: look to see if coordinator should send or not
-                _unauthorized_message()
-        except requests.ConnectionError:
-            _coordinator_connection_failure()
-
-
-        #if the coordinator issues a response, cache the worker routes
-        #and return true
-        if resp.status_code == httplib.OK:
-            routes = resp.json()
-
     def on_post(self, req, resp, tenant_id):
 
         #Validate the tenant's JSON event log data as valid JSON.
         body = load_body(req)
         message_token = req.get_header(MSG_TOKEN)
 
-        # create cache
+        # call to coordinator
         cache = NativeProxy()
+
+        #attempt to validate message token from the cache
         if cache.cache_exists(tenant_id):
-            message_token == cache.cache_get(tenant_id)
+            tenant_info = jsonutils.loads(cache.cache_get(tenant_id))
 
-        #validate that tenant is cache locally
-        #todo: verify how much of the tenant object is in local cache
-        tenant = self._get_tenant_from_cache(tenant_id)
+            # Message token not valid abort
+            if tenant_info['message_token'] != message_token:
+                _unauthorized_message()
 
-        #call to coordinator api
-        if not tenant:
-            self._get_tenant_from_coordinator(tenant_id, message_token)
+            # extract tenant object from cache
+            tenant = load_tenant_from_dict(tenant_info['tenant_object'])
 
+        else:
+            # build head from cache to validate message token and tenant id
+            config = jsonutils.loads(cache.cache_get('worker_configuration'))
+            token_header = {"MESSAGE-TOKEN": message_token,
+                            "WORKER-ID": config['worker_id'],
+                            "WORKER-TOKEN": config['worker_token']
+                            }
+            request_uri = "{0}/{1}/token".format(
+                config['coordinator_uri'], tenant_id)
 
-        if not tenant:
-            _tenant_not_found()
+            try:
+                resp = http_request(request_uri, token_header,
+                                    http_verb='HEAD')
+                if resp.status_code != httplib.OK:
+                    _unauthorized_message()
+            except requests.ConnectionError:
+                _coordinator_connection_failure()
 
+            token_header = {"WORKER-ID": config['worker_id'],
+                            "WORKER-TOKEN": config['worker_token']
+                            }
+            request_uri = "{0}/{1}".format(
+                config['coordinator_uri'], tenant_id)
 
+            try:
+                resp = http_request(request_uri, token_header,
+                                    http_verb='GET')
+                if resp.status_code == httplib.OK:
+                    response_body = resp.json()
+                    tenant = load_tenant_from_dict(response_body['tenant'])
+                else:
+                    _coordinator_connection_failure()
+            except requests.ConnectionError:
+                _coordinator_connection_failure()
 
-        resp.status = falcon.HTTP_202
+        # validate host with tenant
+        if 'hostname' not in body.keys() or not body['hostname']:
+            _hostname_not_provided()
+
+        host = find_host(tenant, host_name=body['hostname'])
+
+        if not host:
+            _host_not_found()
+
+        if 'procname' not in body.keys() or not body['procname']:
+            _producer_not_provided()
+
+        producer = find_event_producer_for_host(tenant, host, body['procname'])
+
+        if not producer:
+            #todo use default producer
+            pass
+        

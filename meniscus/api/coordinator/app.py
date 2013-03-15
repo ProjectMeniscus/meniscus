d5ce8aca4b95ecd86e143f9ab670855cdb69597b
taz <aelkikhia@gmail.com>
2013-03-15 10:00:35 -0500

    preliminary changes for coorrelation worker and coordinator sprint 3

--- meniscus/api/coordinator/app.py
+++ meniscus/api/coordinator/app.py
@@ -1,21 +1,52 @@
 import falcon
 
-from meniscus.api.coordinator.configuration_resources import *
-from meniscus.api.coordinator.registration_resources import *
-from meniscus.api.coordinator.version_resource import VersionResource
+from meniscus.api.coordinator.configuration_resources \
+    import WorkerConfigurationResource
+from meniscus.api.coordinator.registration_resources \
+    import WorkerRegistrationResource
+from meniscus.api.tenant.resources import VersionResource, \
+    TenantResource, UserResource, HostProfilesResource, HostProfileResource, \
+    EventProducersResource, EventProducerResource, HostsResource, HostResource
+
 from meniscus.api.datastore_init import db_handler
 
 
-# Resources
+#Common Resource(s)
 versions = VersionResource()
-worker_registration = WorkerRegistrationResource(db_handler())
+
+#Coordinator Resources
 worker_configuration = WorkerConfigurationResource(db_handler())
-worker_status = WorkerRegistrationResource(db_handler())
-# Routing
+worker_registration = WorkerRegistrationResource(db_handler())
+
+#Tenant Resources
+tenant = TenantResource(db_handler())
+user = UserResource(db_handler())
+profiles = HostProfilesResource(db_handler())
+profile = HostProfileResource(db_handler())
+event_producers = EventProducersResource(db_handler())
+event_producer = EventProducerResource(db_handler())
+hosts = HostsResource(db_handler())
+host = HostResource(db_handler())
+
+# Create API
 application = api = falcon.API()
 
-
+# Common Routing
 api.add_route('/', versions)
+
+# Coordinator Routing
 api.add_route('/v1/pairing', worker_registration)
 api.add_route('/v1/worker/{worker_id}/configuration', worker_configuration)
-api.add_route('/v1/worker/{worker_id}/status', worker_status)
+api.add_route('/v1/worker/{worker_id}/status', worker_registration)
+
+# Tenant Routing
+api.add_route('/v1', tenant)
+api.add_route('/v1/{tenant_id}', user)
+api.add_route('/v1/{tenant_id}/profiles', profiles)
+api.add_route('/v1/{tenant_id}/profiles/{profile_id}', profile)
+api.add_route('/v1/{tenant_id}/producers', event_producers)
+api.add_route('/v1/{tenant_id}/producers/{event_producer_id}',
+              event_producer)
+api.add_route('/v1/{tenant_id}/hosts', hosts)
+api.add_route('/v1/{tenant_id}/hosts/{host_id}', host)
+
--- meniscus/personas/worker/correlation/__init__.py
+++ meniscus/personas/worker/correlation/__init__.py
@@ -1,5 +1,6 @@
 import falcon
 from meniscus.personas.worker.register_online import RegisterWorkerOnline
+from meniscus.personas.worker.correlation.resources import PublishResource
 
 
 def start_up(cfg=dict()):
@@ -9,5 +10,8 @@ def start_up(cfg=dict()):
 
     register_worker_online = RegisterWorkerOnline()
     register_worker_online.run()
+    publish = PublishResource()
+
+    api.add_route('/v1/{tenant_id}/publish', publish)
 
     return application

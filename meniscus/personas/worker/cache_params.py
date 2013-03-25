244e0f273d59d29f67e51c48cb183c5d0647c86d
Steven Gonzales <stevendgonzales@gmail.com>
2013-03-25 11:47:29 -0500

    changes to cache_params to set default values using oslo-config

--- meniscus/personas/worker/cache_params.py
+++ meniscus/personas/worker/cache_params.py
@@ -1,21 +1,44 @@
+from oslo.config import cfg
+
 from meniscus.config import get_config
 from meniscus.config import init_config
-from oslo.config.cfg import ConfigFilesNotFoundError
 
-try:
-    init_config()
-    conf = get_config()
 
-    DEFAULT_EXPIRES = conf.cache.default_expires
-    CONFIG_EXPIRES = conf.cache.config_expires
-    CACHE_CONFIG = conf.cache.cache_config
-    CACHE_TENANT = conf.cache.cache_tenant
-    CACHE_TOKEN = conf.cache.cache_token
+# cache configuration options
+_cache_group = cfg.OptGroup(name='cache', title='Cache Options')
+get_config().register_group(_cache_group)
 
-except ConfigFilesNotFoundError:
+_CACHE_OPTIONS = [
+    cfg.ListOpt('default_expires',
+                default=900,
+                help="""default time to keep items in cache"""
+                ),
+    cfg.StrOpt('config_expires',
+               default=0,
+               help="""Default time to keep worker config items in cache."""
+               ),
+    cfg.StrOpt('cache_config',
+               default='cache-config',
+               help="""The name of the cache to store worker config values"""
+               ),
+    cfg.StrOpt('cache_tenant',
+               default='cache-tenant',
+               help="""The name of the cache to store worker config values"""
+    ),
+    cfg.StrOpt('cache_token',
+               default='cache-token',
+               help="""The name of the cache to store worker config values"""
+    )
+]
 
-    DEFAULT_EXPIRES = 900
-    CONFIG_EXPIRES = 0
-    CACHE_CONFIG = 'cache-config'
-    CACHE_TENANT = 'cache-tenant'
-    CACHE_TOKEN = 'cache-token'
+get_config().register_opts(_CACHE_OPTIONS, group=_cache_group)
+
+init_config()
+conf = get_config()
+
+DEFAULT_EXPIRES = conf.cache.default_expires
+CONFIG_EXPIRES = conf.cache.config_expires
+CACHE_CONFIG = conf.cache.cache_config
+CACHE_TENANT = conf.cache.cache_tenant
+CACHE_TOKEN = conf.cache.cache_token
+

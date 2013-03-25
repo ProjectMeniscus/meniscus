ec162b46ee29146de81e61042effee626987ed6d
Steven Gonzales <stevendgonzales@gmail.com>
2013-03-25 16:08:00 -0500

    app file for correlation personality

--- meniscus/personas/worker/correlation/app.py
+++ meniscus/personas/worker/correlation/app.py
@@ -1,12 +1,21 @@
 import falcon
 
 from meniscus.personas.worker.register_online import RegisterWorkerOnline
+from meniscus.proxy import NativeProxy
+from resources import PublishMessageResource
+from resources import VersionResource
 
 
 def start_up(cfg=dict()):
+    cache = NativeProxy()
+    versions = VersionResource()
+    publish_message = PublishMessageResource(cache)
 
     # Routing
     application = api = falcon.API()
+
+    api.add_route('/', versions)
+    api.add_route('/{tenant_id}/publish', publish_message)
 
     register_worker_online = RegisterWorkerOnline()
     register_worker_online.run()

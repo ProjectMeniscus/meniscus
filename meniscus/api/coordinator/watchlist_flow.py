9266b1bbe019f491097e9157ce51e7f5218585ee
Almutaz Elkikhia <taz@corsair.(none)>
2013-04-12 12:36:15 -0500

    watchlist unittesting

--- meniscus/api/coordinator/watchlist_flow.py
+++ meniscus/api/coordinator/watchlist_flow.py
@@ -1,13 +1,20 @@
 
+import httplib
+
 from datetime import datetime, timedelta
 
+import requests
+
+from meniscus.api.utils.request import http_request
 from oslo.config import cfg
 from meniscus.api.coordinator import coordinator_flow
+from meniscus.api.coordinator import coordinator_errors
 from meniscus.api.personalities import PERSONALITIES
 from meniscus.config import get_config
 from meniscus.config import init_config
 from meniscus.data.model.worker import WatchlistItem
 from meniscus.data.model.worker import Worker
+from meniscus.openstack.common import jsonutils
 
 
 # cache configuration options
@@ -18,12 +25,10 @@ get_config().register_group(_WATCHLIST_GROUP)
 _WATCHLIST_THRESHOLDS = [
     cfg.IntOpt('failure_tolerance_seconds',
                default=60,
-               help="""default duration for monitoring failed workers"""
-               ),
+               help="""default duration for monitoring failed workers"""),
     cfg.IntOpt('watchlist_count_threshold',
                default=5,
-               help="""count of reported failures"""
-               )
+               help="""count of reported failures""")
 ]
 
 get_config().register_opts(_WATCHLIST_THRESHOLDS, group=_WATCHLIST_GROUP)
@@ -67,7 +72,18 @@ def _delete_expired_watchlist_items(db):
     db.delete('watchlist', {'last_changed': {'$lt': threshold}})
 
 
-def _build_broadcast_config_change(db, worker):
+def _get_broadcaster_list(db):
+    broadcaster_list = db.find(
+        'worker', {'personality': 'broadcaster',
+                   'status': {'$in': coordinator_flow.VALID_ROUTE_LIST}})
+
+    broadcaster_workers = [Worker(**worker).ip_address_v4
+                           for worker in broadcaster_list]
+
+    return broadcaster_workers
+
+
+def _get_broadcast_targets(db, worker):
     """
     builds list of worker callback addresses for config change broadcast
     """
@@ -82,19 +98,47 @@ def _build_broadcast_config_change(db, worker):
     upstream_workers = [Worker(**worker) for worker in upstream_list]
 
     broadcast = {
-        'type': 'ROUTES',
-        'targets': [target.callback for target in upstream_workers]
+        "type": "ROUTES",
+        "targets": [target.ip_address_v4 for target in upstream_workers]
     }
 
-    return {'broadcast': broadcast}
+    if len(broadcast['targets']):
+        return {"broadcast": broadcast}
+
+    return []
 
 
-def _send_callback_list_to_broadcast(db, worker):
+def _send_target_list_to_broadcaster(db, worker):
     """
     send broadcast list to broadcaster
     """
-    broadcast = _build_broadcast_config_change(db, worker)
-    pass
+
+    # list of broadcasters
+    broadcasters = _get_broadcaster_list(db)
+    if not broadcasters:
+        ## todo log no online broadcasters
+        return False
+
+    # list of broadcast targets
+    broadcast_targets = _get_broadcast_targets(db, worker)
+    if not broadcast_targets:
+        ## todo: log failed to find target
+        return False
+
+    for broadcaster_uri in broadcasters:
+        resp = None
+        try:
+            ## todo refactor callback address to not include /v1/callback/"
+            resp = http_request(
+                '{0}:8080/v1/broadcast'.format(broadcaster_uri),
+                jsonutils.dumps(broadcast_targets), http_verb='PUT')
+        except requests.RequestException:
+            raise coordinator_errors.BroadcasterCommunicationError
+        if resp.status_code == httplib.OK:
+            return True
+    else:
+        ## todo Log broadcaster connection failure
+        return False
 
 
 def process_watchlist_item(db, worker_id):
@@ -117,6 +161,6 @@ def process_watchlist_item(db, worker_id):
 
             if worker.status != 'offline':
                 coordinator_flow.update_worker_status(db, worker, 'offline')
-                _send_callback_list_to_broadcast(db, worker)
+                _send_target_list_to_broadcaster(db, worker)
 
         _update_watchlist_item(db, watch_dict)

208c8a211fa1ca7b4ca3050542b8000b27581767
Steven Gonzales <stevendgonzales@gmail.com>
2013-05-25 19:10:12 -0500

    celery config options

--- meniscus/api/storage/persistence.py
+++ meniscus/api/storage/persistence.py
@@ -1,13 +1,13 @@
 from meniscus.api.datastore_init import db_handler
 from meniscus.queue import celery
-from pyes.connection_http import NoServerAvailable
 
 
-@celery.task(acks_late=True, ignore_result=True, serializer="json")
+@celery.task(acks_late=True, max_retries=0,
+             ignore_result=True, serializer="json")
 def persist_message(message):
     """Takes a message dict and persists it to the configured database."""
     try:
         _sink = db_handler()
         _sink.put('logs', message)
-    except NoServerAvailable:
+    except:
         persist_message.retry()
--- meniscus/queue/__init__.py
+++ meniscus/queue/__init__.py
@@ -1,3 +1,5 @@
 from celery import Celery
 
-celery = Celery('meniscus', broker='librabbitmq://guest@localhost//')
+celery = Celery('meniscus',
+                broker='librabbitmq://guest@localhost//',
+                config_source="meniscus.queue.celery_config")

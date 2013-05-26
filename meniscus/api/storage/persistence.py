ffd59222c3269c4036746f454eac75d7862dd3ad
Steven Gonzales <stevendgonzales@gmail.com>
2013-05-25 20:39:27 -0500

    elastic search thread safe

--- meniscus/api/storage/persistence.py
+++ meniscus/api/storage/persistence.py
@@ -11,6 +11,9 @@ def persist_message(message):
     """Takes a message dict and persists it to the configured database."""
     try:
         _sink = datastore_init.db_handler()
+        _sink.close()
+        _sink.connect()
         _sink.put('logs', message)
+        _sink.close()
     except:
         persist_message.retry()

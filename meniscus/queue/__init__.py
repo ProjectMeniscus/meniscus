2468dc13edc34f791bc7464ac56a20ccb927398a
Steven Gonzales <stevendgonzales@gmail.com>
2013-05-25 20:11:31 -0500

    testing eventlet

--- meniscus/queue/__init__.py
+++ meniscus/queue/__init__.py
@@ -3,5 +3,6 @@ from celery import Celery
 celery = Celery('meniscus',
                 broker='librabbitmq://guest@localhost//')
 
+celery.conf.CELERYD_POOL = 'eventlet'
 celery.conf.CELERYD_CONCURRENCY = 100
-CELERY_DISABLE_RATE_LIMITS = True
+celery.conf.CELERY_DISABLE_RATE_LIMITS = True

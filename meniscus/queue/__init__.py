7fdf75cdba8b1b255311fa169c9fc567b436d970
Steven Gonzales <stevendgonzales@gmail.com>
2013-05-25 19:28:58 -0500

    celer config cnahges for concurrency

--- meniscus/queue/__init__.py
+++ meniscus/queue/__init__.py
@@ -1,5 +1,7 @@
 from celery import Celery
 
 celery = Celery('meniscus',
-                broker='librabbitmq://guest@localhost//',
-                config_source="meniscus.queue.celery_config")
+                broker='librabbitmq://guest@localhost//')
+
+celery.conf.CELERYD_CONCURRENCY = 10
+CELERY_DISABLE_RATE_LIMITS = True
--- meniscus/queue/celery_config.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-sys.path.append('.')
-
-CELERYD_CONCURRENCY = 10
-#CELERYD_POOL = 'eventlet'
-CELERY_DISABLE_RATE_LIMITS = True

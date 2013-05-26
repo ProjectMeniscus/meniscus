2cc1c3d751739727f0daa39afdb3400fbc0eedc2
Steven Gonzales <stevendgonzales@gmail.com>
2013-05-25 22:22:32 -0500

    eventlet

--- meniscus/api/storage/persistence.py
+++ meniscus/api/storage/persistence.py
@@ -1,16 +1,15 @@
-
-#from meniscus.api.datastore_init import db_handler
 import eventlet
-from meniscus.queue import celery
-datastore_init = eventlet.import_patched('meniscus.api.datastore_init')
 
+from meniscus.api.datastore_init import db_handler
+from meniscus.queue import celery
+eventlet.monkey_patch()
 
 @celery.task(acks_late=True, max_retries=0,
              ignore_result=True, serializer="json")
 def persist_message(message):
     """Takes a message dict and persists it to the configured database."""
     #try:
-    _sink = datastore_init.db_handler()
+    _sink = db_handler()
     _sink.close()
     _sink.connect()
     _sink.put('logs', message)

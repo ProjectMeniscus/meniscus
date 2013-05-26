358f275362be58def1b7cfbf3a623cbd5b3083ba
Steven Gonzales <stevendgonzales@gmail.com>
2013-05-25 20:52:52 -0500

    uncomment retry

--- meniscus/api/storage/persistence.py
+++ meniscus/api/storage/persistence.py
@@ -9,11 +9,11 @@ datastore_init = eventlet.import_patched('meniscus.api.datastore_init')
              ignore_result=True, serializer="json")
 def persist_message(message):
     """Takes a message dict and persists it to the configured database."""
-    try:
-        _sink = datastore_init.db_handler()
-        _sink.close()
-        _sink.connect()
-        _sink.put('logs', message)
-        _sink.close()
-    except:
-        persist_message.retry()
+    #try:
+    _sink = datastore_init.db_handler()
+    _sink.close()
+    _sink.connect()
+    _sink.put('logs', message)
+    _sink.close()
+    #except:
+     #   persist_message.retry()

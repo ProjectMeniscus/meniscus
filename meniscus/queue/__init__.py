b4497a10a2b44908ca58dba8e0b7b93b58d18e2b
Steven Gonzales <stevendgonzales@gmail.com>
2013-05-25 22:28:28 -0500

    lower concurrency

--- meniscus/queue/__init__.py
+++ meniscus/queue/__init__.py
@@ -4,5 +4,5 @@ celery = Celery('meniscus',
                 broker='librabbitmq://guest@localhost//')
 
 celery.conf.CELERYD_POOL = 'eventlet'
-celery.conf.CELERYD_CONCURRENCY = 100
+celery.conf.CELERYD_CONCURRENCY = 25
 celery.conf.CELERY_DISABLE_RATE_LIMITS = True

c891b2230cdffe20a6fafd4b3cfb0714d8814a7d
Steven Gonzales <stevendgonzales@gmail.com>
2013-05-28 10:49:09 -0500

    pep8 cleanup

--- meniscus/queue/__init__.py
+++ meniscus/queue/__init__.py
@@ -11,19 +11,19 @@ _CELERY = [
     cfg.StrOpt('BROKER_URL',
                default="librabbitmq://guest@localhost//",
                help="""url to the broker behind celery"""
-    ),
+               ),
     cfg.IntOpt('CELERYD_CONCURRENCY',
                default=10,
                help="""Number of concurrent worker processes/threads"""
-    ),
+               ),
     cfg.BoolOpt('CELERY_DISABLE_RATE_LIMITS',
                 default=True,
                 help="""disable celery rate limit"""
-    ),
-    cfg.StrOpt( 'CELERY_TASK_SERIALIZER',
+                ),
+    cfg.StrOpt('CELERY_TASK_SERIALIZER',
                default="json",
                help="""default serialization method to use"""
-    )
+               )
 ]
 
 config.get_config().register_opts(_CELERY, group=_CELERY_GROUP)

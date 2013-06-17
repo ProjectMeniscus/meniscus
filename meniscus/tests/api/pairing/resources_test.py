bbc5aaf27810b8762cfa051d01c86513d38644d0
Steven Gonzales <stevendgonzales@gmail.com>
2013-06-17 13:11:50 -0500

    add json validation for pairing_configuration

--- meniscus/tests/api/pairing/resources_test.py
+++ meniscus/tests/api/pairing/resources_test.py
@@ -1,11 +1,11 @@
 import unittest
 
 import falcon
-
+import falcon.testing as testing
 from mock import MagicMock
 from mock import patch
 
-
+from meniscus.openstack.common import jsonutils
 from meniscus.api.pairing.resources import PairingConfigurationResource
 
 
@@ -15,21 +15,68 @@ def suite():
     return suite
 
 
-class WhenTestingPairingConfigurationResource(unittest.TestCase):
-    def setUp(self):
-        self.req = MagicMock()
-        self.resp = MagicMock()
+class WhenTestingPairingConfigurationResource(testing.TestBase):
+    def before(self):
+        self.configuration = {
+            'pairing_configuration': {
+                "api_secret": "ce20a1f3-151b-4302-ad42-52d91349fe8b",
+                "coordinator_uri": "http://localhost:8080/v1",
+                "personality": "worker"
+            }
+        }
+        self.configuration_bad_secret = {
+            'pairing_configuration': {
+                "api_secret": "this is not a uuid",
+                "coordinator_uri": "http://localhost:8080/v1",
+                "personality": "worker"
+            }
+        }
+        self.configuration_bad_personality = {
+            'pairing_configuration': {
+                "api_secret": "ce20a1f3-151b-4302-ad42-52d91349fe8b",
+                "coordinator_uri": "http://localhost:8080/v1",
+                "personality": "invalid_personality"
+            }
+        }
         self.resource = PairingConfigurationResource()
+        self.test_route = '/v1/pairing/configure'
+        self.api.add_route(self.test_route, self.resource)
 
-    def test_should_return_200_on_post(self):
-        self.req.stream.read.return_value = \
-            u'{ "api_secret" : "1234", ' \
-            u'"coordinator_uri" : "http://localhost:8080/v1" , ' \
-            u'"personality" : "1234"  }'
+    def test_should_return_400_on_bad_secret(self):
         with patch('meniscus.api.pairing.resources.PairingProcess',
                    MagicMock()):
-            self.resource.on_post(self.req, self.resp)
-        self.assertEquals(self.resp.status, falcon.HTTP_200)
+            self.simulate_request(
+                self.test_route,
+                method='POST',
+                headers={
+                    'content-type': 'application/json',
+                },
+                body=jsonutils.dumps(self.configuration_bad_secret))
+        self.assertEqual(falcon.HTTP_400, self.srmock.status)
+
+    def test_should_return_400_on_bad_personality(self):
+        with patch('meniscus.api.pairing.resources.PairingProcess',
+                   MagicMock()):
+            self.simulate_request(
+                self.test_route,
+                method='POST',
+                headers={
+                    'content-type': 'application/json',
+                },
+                body=jsonutils.dumps(self.configuration_bad_personality))
+        self.assertEqual(falcon.HTTP_400, self.srmock.status)
+
+    def test_should_return_200_on_post(self):
+        with patch('meniscus.api.pairing.resources.PairingProcess',
+                   MagicMock()):
+            self.simulate_request(
+                self.test_route,
+                method='POST',
+                headers={
+                    'content-type': 'application/json',
+                },
+                body=jsonutils.dumps(self.configuration))
+        self.assertEqual(falcon.HTTP_200, self.srmock.status)
 
 if __name__ == '__main__':
     unittest.main()

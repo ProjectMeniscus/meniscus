cf597ac897ff28d4ab0676567eeedfd5ad797343
John Hopper <john.hopper@rackspace.com>
2013-06-19 17:20:30 -0500

    more work on the build script

--- build.py
+++ build.py
@@ -182,8 +182,9 @@ def build(requirements_file, hooks, project_name, version):
     local_etc = path.join('.', 'etc')
     copytree(local_etc, bctx.etc)
 
+    project_etc = path.join(bctx.etc, project_name)
     uwsgi_ini = path.join('.', 'uwsgi.ini')
-    copytree(local_etc, path.join(bctx.etc, 'uwsgi.ini'))
+    shutil.copyfile(uwsgi_ini, path.join(project_etc, 'uwsgi.ini'))
 
     init_script_name = '{}.init'.format(project_name)
     init_script_path = path.join(path.join('.', 'pkg'), init_script_name)

07e81646b940f569d562d3ecd0187afa98f24b1f
Steven Gonzales <stevendgonzales@gmail.com>
2013-06-25 15:02:11 -0500

    update default sink

--- meniscus/tests/sinks/test_elasticsearch.py
+++ meniscus/tests/sinks/test_elasticsearch.py
@@ -2,7 +2,7 @@ import unittest
 
 from mock import MagicMock, patch
 
-from meniscus.sinks.elasticsearch import persist_message
+from meniscus.sinks.default_sink import persist_message
 
 
 class WhenTestingStoragePersistence(unittest.TestCase):

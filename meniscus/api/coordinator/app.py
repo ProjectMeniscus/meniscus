7e1c26693f858c225ef7ed831f47ff69f8122e43
Steven Gonzales <stevendgonzales@gmail.com>
2013-06-25 10:18:09 -0500

    debug

--- meniscus/api/coordinator/app.py
+++ meniscus/api/coordinator/app.py
@@ -16,6 +16,12 @@ from meniscus.api.version.resources import VersionResource
 
 from meniscus.api.datastore_init import db_handler
 
+from meniscus import env
+from meniscus.openstack.common import log
+
+
+log.setup('meniscus')
+_LOG = env.get_logger(__name__)
 
 #Common Resource(s)
 versions = VersionResource()

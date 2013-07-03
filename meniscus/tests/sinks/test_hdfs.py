09410bbb425ba63dea4e67810fcb13901c3dfa11
Steven Gonzales <stevendgonzales@gmail.com>
2013-07-03 16:31:43 -0500

    unit tests for HDFS transactions

--- meniscus/tests/sinks/test_hdfs.py
+++ meniscus/tests/sinks/test_hdfs.py
@@ -1,3 +1,4 @@
+from datetime import date
 import unittest
 import uuid
 
@@ -5,150 +6,41 @@ from mock import MagicMock, patch
 _db_handler = MagicMock()
 with patch('meniscus.data.datastore.datasource_handler',
            MagicMock(return_value=_db_handler)):
-    from meniscus.storage import transaction
+    from meniscus.sinks import hdfs
 
 
-class WhenTestingTransaction(unittest.TestCase):
-
-    def setUp(self):
-        self.transaction = transaction.Transaction()
-
-    def test_lock_records(self):
-        with self.assertRaises(NotImplementedError):
-            self.transaction._lock_records()
-
-    def test_retrieve_locked_records(self):
-        with self.assertRaises(NotImplementedError):
-            self.transaction._retrieve_locked_records()
-
-    def test_process_locked_records(self):
-        with self.assertRaises(NotImplementedError):
-            self.transaction._process_locked_records()
-
-    def test_release_lock(self):
-        with self.assertRaises(NotImplementedError):
-            self.transaction._release_lock()
-
-    def test_process_transaction(self):
-        with self.assertRaises(NotImplementedError):
-            self.transaction.process_transaction()
-
-
-class WhenTestingBatchMessageTransaction(unittest.TestCase):
+class WhenTestingHdfsTransaction(unittest.TestCase):
     def setUp(self):
         self.transaction_id = str(uuid.uuid4())
         self.expire_seconds = 60
-        self.sink_name = 'test_sink'
-        self.time = 1372878456.223144
-        with patch('meniscus.storage.transaction.time.time',
-                   MagicMock(return_value=self.time)):
-            self.transaction = transaction.BatchMessageTransaction(
-                self.sink_name, self.transaction_id, self.expire_seconds)
+        self.sink_name = 'hdfs'
+        self.hdfs_transaction = hdfs.HdfsTransaction(
+            self.sink_name, self.transaction_id, self.expire_seconds)
+        self.hdfs_transaction.locked_records = ['record']
 
-    def test_constructor(self):
-        self.assertEqual(self.transaction.transaction_id, self.transaction_id)
-        self.assertEqual(
-            self.transaction.transaction_field,
-            "meniscus.correlation.destinations.{0}".format(self.sink_name))
-        self.assertEqual(
-            self.transaction.id_field,
-            "{0}.transaction_id".format(self.transaction.transaction_field))
-        self.assertEqual(
-            self.transaction.time_field,
-            "{0}.transaction_time".format(self.transaction.transaction_field))
-        self.assertEqual(self.transaction.expire_seconds, self.expire_seconds)
+    def test_process_locked_records_creates_file(self):
+        write_dir = "{0}/{1}".format(
+            hdfs.conf.hdfs_sink.base_directory,
+            (date.fromtimestamp(
+                self.hdfs_transaction.transaction_time)).isoformat())
+        file_path = "{0}/{1}".format(write_dir, self.transaction_id)
+        create_file = MagicMock()
+        with patch.object(hdfs.webhdfs.PyWebHdfsClient,
+                          'create_file', create_file):
+            self.hdfs_transaction._process_locked_records()
+        create_file.assert_called_once_with(file_path, '"record"')
 
-    def test_lock_records(self):
-
-        self.transaction._lock_records()
-        _db_handler.set_field.assert_called_once_with(
-            'logs',
-            {
-                self.transaction.time_field: self.time,
-                self.transaction.id_field: self.transaction_id
-            },
-            {
-                '$and': [
-                    {
-                        self.transaction.transaction_field: {'$exists': True}
-                    },
-                    {
-                        '$or': [
-                            {self.transaction.time_field:
-                                {'$lt': self.transaction.threshold_time}
-                             },
-                            {
-                                self.transaction.time_field: None
-                            }
-                        ]
-                    }
-                ]
-            }
-        )
-
-    def test_retreive_locked_records(self):
-        self.transaction._retrieve_locked_records()
-        _db_handler.find.assert_called_once_with(
-            'logs',
-            {self.transaction.id_field: self.transaction.transaction_id},
-            {"_id": False}
-        )
-
-    def test_release_lock(self):
-        lock_task = MagicMock()
-        lock_task.delay = MagicMock()
-        with patch(
-                'meniscus.storage.transaction._release_lock_task', lock_task):
-            self.transaction._release_lock()
-        lock_task.delay.assert_called_once_with(
-            self.transaction.transaction_field,
-            self.transaction.id_field,
-            self.transaction.transaction_id)
-
-    def test_process_locked_records(self):
-        with self.assertRaises(NotImplementedError):
-            self.transaction._process_locked_records()
-
-    def test_process_transaction(self):
-        _lock_records = MagicMock()
-        _retrieve_locked_records = MagicMock()
-        _process_locked_records = MagicMock()
-        _release_lock = MagicMock()
-        locked_records = MagicMock()
-        locked_records.count.return_value = 1
-
-        self.transaction._lock_records = _lock_records
-        self.transaction._retrieve_locked_records = _retrieve_locked_records
-        self.transaction._process_locked_records = _process_locked_records
-        self.transaction._release_lock = _release_lock
-        self.transaction.locked_records = locked_records
-
-        self.transaction.process_transaction()
-        _lock_records.assert_called_once()
-        _retrieve_locked_records.assert_called_once()
-        _process_locked_records.assert_called_once()
-        _release_lock.assert_called_once()
-
-    def test_handler_remove_field_called(self):
-        remove_field = {self.transaction.transaction_field: ""}
-        update_filter = {
-            self.transaction.id_field: self.transaction.transaction_id
-        }
-        transaction._release_lock_task(
-            self.transaction.transaction_field,
-            self.transaction.id_field,
-            self.transaction.transaction_id)
-
-        _db_handler.remove_field.assert_called_once_with(
-            "logs", remove_field, update_filter)
-
-    def test_retry_called_on_exception(self):
-        _db_handler.remove_field.side_effect = Exception
-        transaction._release_lock_task.retry = MagicMock()
-
-        transaction._release_lock_task(
-            self.transaction.transaction_field,
-            self.transaction.id_field,
-            self.transaction.transaction_id)
-
-        transaction._release_lock_task.retry.assert_called_once()
+    def test_process_locked_records_handles_exception(self):
+        write_dir = "{0}/{1}".format(
+            hdfs.conf.hdfs_sink.base_directory,
+            (date.fromtimestamp(
+                self.hdfs_transaction.transaction_time)).isoformat())
+        file_path = "{0}/{1}".format(write_dir, self.transaction_id)
+        make_dir = MagicMock()
+        create_file = MagicMock(
+            side_effect=[hdfs.webhdfs.errors.FileNotFound, True])
+        with patch.object(hdfs.webhdfs.PyWebHdfsClient,
+                          'create_file', create_file), patch.object(
+                hdfs.webhdfs.PyWebHdfsClient, 'make_dir', make_dir):
+            self.hdfs_transaction._process_locked_records()
+        make_dir.assert_called_once_with(write_dir)

55d75765d38c8f6e88fcb9e1f77303747dec6eef
Steven Gonzales <stevendgonzales@gmail.com>
2013-07-24 09:18:19 -0500

    make registering of data_handlers optional

--- meniscus/data/datastore/__init__.py
+++ meniscus/data/datastore/__init__.py
@@ -18,6 +18,12 @@ coordinator_db_group = cfg.OptGroup(
 config.get_config().register_group(coordinator_db_group)
 
 coordinator_db_options = [
+    cfg.BoolOpt('active',
+                default=True,
+                help="""Determines whether the handler for this store
+                        is registered for use
+                     """
+                ),
     cfg.StrOpt('adapter_name',
                default='mongodb',
                help="""Sets the name of the handler to load for
@@ -63,6 +69,12 @@ short_term_store_group = cfg.OptGroup(
 config.get_config().register_group(short_term_store_group)
 
 short_term_store_options = [
+    cfg.BoolOpt('active',
+                default=False,
+                help="""Determines whether the handler for this store
+                        is registered for use
+                     """
+                ),
     cfg.StrOpt('adapter_name',
                default='mongodb',
                help="""Sets the name of the handler to load for
@@ -108,6 +120,12 @@ default_sink_group = cfg.OptGroup(
 config.get_config().register_group(default_sink_group)
 
 default_sink_options = [
+    cfg.BoolOpt('active',
+                default=True,
+                help="""Determines whether the handler for this store
+                       is registered for use
+                    """
+                ),
     cfg.StrOpt('adapter_name',
                default='elasticsearch',
                help="""Sets the name of the handler to load for
@@ -160,28 +178,41 @@ conf = config.get_config()
 # Handler registration
 _DATASOURCE_HANDLERS = handler.DatasourceHandlerManager()
 
-#create coordinator_db handler
-coordinator_db_module = import_module(
-    'meniscus.data.adapters.{0}'.format(conf.coordinator_db.adapter_name))
-coordinator_db_handler = coordinator_db_module.NamedDatasourceHandler(
-    conf.coordinator_db)
-_DATASOURCE_HANDLERS.register(COORDINATOR_DB, coordinator_db_handler)
+
+if conf.coordinator_db.active:
+    #create coordinator_db handler
+    coordinator_db_module = import_module(
+        'meniscus.data.adapters.{0}'.format(conf.coordinator_db.adapter_name))
+
+    coordinator_db_handler = coordinator_db_module.NamedDatasourceHandler(
+        conf.coordinator_db)
+    _DATASOURCE_HANDLERS.register(COORDINATOR_DB, coordinator_db_handler)
+    #if handler is not active, register as None
+else:
+    _DATASOURCE_HANDLERS.register(COORDINATOR_DB, None)
 
 
-#create short_term_store handler
-short_term_store_module = import_module(
-    'meniscus.data.adapters.{0}'.format(conf.short_term_store.adapter_name))
-short_term_store_handler = short_term_store_module.NamedDatasourceHandler(
-    conf.short_term_store)
-_DATASOURCE_HANDLERS.register(SHORT_TERM_STORE, short_term_store_handler)
+if conf.short_term_store.active:
+    #create short_term_store handler
+    short_term_store_module = import_module(
+        'meniscus.data.adapters.{0}'.format(
+            conf.short_term_store.adapter_name))
+    short_term_store_handler = short_term_store_module.NamedDatasourceHandler(
+        conf.short_term_store)
+    _DATASOURCE_HANDLERS.register(SHORT_TERM_STORE, short_term_store_handler)
+else:
+    _DATASOURCE_HANDLERS.register(SHORT_TERM_STORE, None)
 
 
-#create default_sink handler
-default_sink_module = import_module(
-    'meniscus.data.adapters.{0}'.format(conf.default_sink.adapter_name))
-default_sink_handler = default_sink_module.NamedDatasourceHandler(
-    conf.default_sink)
-_DATASOURCE_HANDLERS.register(DEFAULT_SINK, default_sink_handler)
+if conf.default_sink.active:
+    #create default_sink handler
+    default_sink_module = import_module(
+        'meniscus.data.adapters.{0}'.format(conf.default_sink.adapter_name))
+    default_sink_handler = default_sink_module.NamedDatasourceHandler(
+        conf.default_sink)
+    _DATASOURCE_HANDLERS.register(DEFAULT_SINK, default_sink_handler)
+else:
+    _DATASOURCE_HANDLERS.register(DEFAULT_SINK, None)
 
 
 def datasource_handler(handler_name):

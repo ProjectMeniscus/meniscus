6baeae18bb4db1e1bd495758d9868f8aa5a7f247
John Hopper <john.hopper@rackspace.com>
2013-08-12 18:40:15 -0500

    reverting to limiting retries

--- meniscus/storage/default_store.py
+++ meniscus/storage/default_store.py
@@ -2,23 +2,19 @@ from meniscus.data.datastore import datasource_handler, DEFAULT_SINK
 from meniscus import env
 from meniscus.queue import celery
 
-from pyes.exceptions import NoServerAvailable
-
 
 _LOG = env.get_logger(__name__)
 
 _db_handler = datasource_handler(DEFAULT_SINK)
 
 
-@celery.task(acks_late=True, max_retries=None,
+@celery.task(acks_late=True, max_retries=3,
              ignore_result=True, serializer="json")
 def persist_message(message):
     """Takes a message and persists it to the default datastore."""
     try:
         sink = _db_handler
         sink.put('tenant/{}'.format(message['meniscus']['tenant']), message)
-    except NoServerAvailable as nsa:
-        _LOG.exception(nsa.message)
-        persist_message.retry()
     except Exception as ex:
         _LOG.exception(ex.message)
+        persist_message.retry()

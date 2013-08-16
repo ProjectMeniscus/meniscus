5c2ef1693cc3bbc53b3efef8b944de6651cb4d29
Steven Gonzales <stevendgonzales@gmail.com>
2013-08-16 13:08:14 -0500

    reinstate indefinite retry in persist_message

--- meniscus/storage/default_store.py
+++ meniscus/storage/default_store.py
@@ -8,7 +8,7 @@ _LOG = env.get_logger(__name__)
 _db_handler = datasource_handler(DEFAULT_SINK)
 
 
-@celery.task(acks_late=True, max_retries=3,
+@celery.task(acks_late=True, max_retries=None,
              ignore_result=True, serializer="json")
 def persist_message(message):
     """Takes a message and persists it to the default datastore."""

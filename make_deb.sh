8bb1f21d5703cce94ad7cbe95166793103196e96
chadlung <chad.lung@gmail.com>
2013-11-15 09:03:03 -0800

    Add python-dev to dependencies for the Debian packaging

--- make_deb.sh
+++ make_deb.sh
@@ -4,4 +4,4 @@ PROJECT_NAME="meniscus"
 PROJECT_VERSION="$(cat VERSION)"
 
 python build.py meniscus
-fpm -d python -v "${PROJECT_VERSION}" -n "${PROJECT_NAME}" -t deb --after-install ./pkg/post_install.deb.sh --after-remove ./pkg/post_remove.deb.sh -s tar "./${PROJECT_NAME}_${PROJECT_VERSION}.tar.gz"
+fpm -d "python" -d "python-dev" -v "${PROJECT_VERSION}" -n "${PROJECT_NAME}" -t deb --after-install ./pkg/post_install.deb.sh --after-remove ./pkg/post_remove.deb.sh -s tar "./${PROJECT_NAME}_${PROJECT_VERSION}.tar.gz"

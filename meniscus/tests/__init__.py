c6a53ab2d5f215a3c76cef2c1e0c69dda12f92b5
chadlung <chad.lung@gmail.com>
2013-11-18 13:50:06 -0600

    Removed un-used code

--- meniscus/tests/__init__.py
+++ meniscus/tests/__init__.py
@@ -1,7 +1,6 @@
 import unittest
 
 import meniscus.tests.config_test
-import meniscus.tests.normalization.drivers.rfc5424_test
 import meniscus.tests.api.tenant.resources_test
 import meniscus.tests.data.datastore.handler_test
 import meniscus.tests.data.adapters.mongodb_test
@@ -10,7 +9,6 @@ import meniscus.tests.ext.plugin_test
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(rfc5424_test.suite())
     suite.addTest(resources_test.suite())
     suite.addTest(config_test.suite())
     suite.addTest(handler_test.suite())

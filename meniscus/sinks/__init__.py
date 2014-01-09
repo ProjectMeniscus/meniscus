016857828b391a2d5730c46014b013f1b1c1bf42
Steven Gonzales <stevendgonzales@gmail.com>
2014-01-09 09:57:10 -0600

    raise sink list into package namespace

--- meniscus/sinks/__init__.py
+++ meniscus/sinks/__init__.py
@@ -1,2 +1,4 @@
-#hoist route_message method into package namespace
+#hoist into package namespace
 from meniscus.sinks.dispatch import route_message
+from meniscus.sinks.dispatch import DEFAULT_SINK
+from meniscus.sinks.dispatch import VALID_SINKS

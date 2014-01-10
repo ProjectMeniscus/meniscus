30db7b96c56f07dad5f5cf51c9f3dae3dd510b80
Steven Gonzales <stevendgonzales@gmail.com>
2014-01-10 14:23:53 -0600

    correct import

--- meniscus/sinks/elasticsearch/__init__.py
+++ meniscus/sinks/elasticsearch/__init__.py
@@ -1,3 +1,3 @@
 #bring put_message task into meniscus.sinks.elasticsearch namespace
 from meniscus.sinks.elasticsearch.sink import put_message
-from meniscus.sinks.elasticsearch.sink import
+from meniscus.sinks.elasticsearch.sink import ElasticSearchStreamBulker
